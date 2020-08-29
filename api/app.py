581e56535a8f31b078f40ffeb40d562d1fb85267
christoffer.karlsson <christoffer.karlsson@netclean.com>
2020-08-29 14:25:50 +0200

    populating db on start

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .database_setup import DATABASE_NAME
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_NAME
@@ -14,6 +15,22 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     barcode = db.Column(db.String(50), nullable=False, index=True, unique=True)
     co2equiv = db.Column(db.Float)
+
+
+def init_db():
+    db.create_all()
+
+    if not Product.query.limit(1).all():
+        crisps = Product(barcode='07340005404889', co2equiv=12.0)
+        db.session.add(crisps)
+
+        biscuits = Product(barcode='07310521050204', co2equiv=2.0)
+        db.session.add(biscuits)
+
+        db.session.commit()
+
+
+init_db()
 
 
 @app.route('/v1/ping')
--- api/populate_database.py
+++ api/populate_database.py
@@ -1,11 +1,3 @@
 import app
 
-app.db.create_all()
 
-crisps = app.Product(barcode='07340005404889', co2equiv=12.0)
-app.db.session.add(crisps)
-
-biscuits = app.Product(barcode='07310521050204', co2equiv=2.0)
-app.db.session.add(biscuits)
-
-app.db.session.commit()

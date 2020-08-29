5541647ebf32baf443b69014c84c5ca9572e299b
Johan Stenberg <johan.stenberg@netclean.com>
2020-08-29 16:09:21 +0200

    Expanded results view

--- app/src/Views/ResultView.tsx
+++ app/src/Views/ResultView.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
 import { ICodeResult } from "../Functions/Api";
 
 interface IResultViewProps {
-  result: any;
+  result: ICodeResult;
   resultCallBack: (result: ICodeResult | null) => void;
 }
 
@@ -14,9 +14,13 @@ export function ResultView(props: IResultViewProps) {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.sortAsLabel}>Recycle as: {props.result.sortAs}</Text>
       <Text style={styles.co2Label}>CO2 Eq: {props.result.co2equiv}</Text>
       <TouchableOpacity style={styles.indicator}>
         <Text style={styles.indicatorText}>Good</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={scanAnotherCode}>
+        <Text style={styles.buttonText}>Vote</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={scanAnotherCode}>
         <Text style={styles.buttonText}>Scan another code</Text>
@@ -27,21 +31,21 @@ export function ResultView(props: IResultViewProps) {
 
 const styles = StyleSheet.create({
   indicator: {
-    height: 40,
+    padding: 8,
     width: "80%",
     backgroundColor: "#3d801f",
-    marginBottom: 24,
-    borderRadius: 2,
+    marginBottom: "50%",
+    borderRadius: 8,
   },
   indicatorText: {
     color: "#FFFFFF",
     textAlign: "center",
   },
 
-  button: { backgroundColor: "#3d801f", padding: 8, borderRadius: 2 },
+  button: { backgroundColor: "#000000", padding: 8, borderRadius: 2, marginBottom: 8 },
   buttonText: { color: "#FFFFFF" },
-
-  co2Label: { marginBottom: 8 },
+  sortAsLabel: { marginBottom: 4, fontSize: 20 },
+  co2Label: { marginBottom: 8, fontSize: 20 },
   container: {
     flex: 1,
     backgroundColor: "#fff",
